df134a9cc423a6e593cb0b9804537e39219f13e8
Denys Bushulyak <apollo128@gmail.com>
2014-06-12 00:38:23 +0300

    Работоспособная версия

---
+++ chain.js
@@ -0,0 +1,53 @@
+function Chain(items) {
+
+    var index = 0;
+
+    var _items = items || [];
+
+    function _next() {
+        index = index > _items.length - 1 ? 0 : index;
+        var item = _items[index];
+        index++;
+        return  item;
+    }
+
+    function _pref() {
+
+        index = index < 0 ? _items.length - 1 : index;
+        var item = _items[index];
+        index--;
+        return  item;
+    }
+
+    function _push(item) {
+        _items.push(item);
+    }
+
+    function _first() {
+        return _items.length > 0 ? _items[0] : undefined;
+    }
+
+    function _last() {
+        return _items.length > 0 ? _items[_items.length - 1] : undefined;
+    }
+
+    function _reset() {
+        index = 0;
+    }
+
+    function _isEnd() {
+        return index === (_items.length);
+    }
+
+    return {
+        next    : _next,
+        prev    : _pref,
+        push    : _push,
+        first   : _first,
+        last    : _last,
+        reset   : _reset,
+        isEnd   : _isEnd,
+        items   : _items,
+        getIndex: function () { return index; }
+    };
+}
